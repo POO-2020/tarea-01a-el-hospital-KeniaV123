6c8d8f7d7b2ba0fae77e7f7179c19559d3dd5a61
Kenia Villa <kvilla4@ucol.mx>
2020-02-18 17:10:41 -0600

    avances

---
+++ nombre.js
@@ -0,0 +1,32 @@
+class Nombre {
+    /*
+
+*/
+
+constructor (nombre, apellidoPaterno, apellidoMaterno){
+    this.nombre = nombre;
+    this.apellidoMaterno = apellidoMaterno;
+    this.apellidoPaterno = apellidoPaterno;
+}
+getNombreCompleto(){
+    return (`${this.nombre} ${this.apellidoPaterno} ${this.apellidoMaterno}`);
+}
+
+getApellidoNombre(){
+    return (`${this.apellidoPaterno} ${this.apellidoMaterno} ${this.nombre}`);
+}
+
+getIniciales(){
+    let inicialNombre = this.nombre [0];
+    let inicialMaterno = this.apellidoMaterno [0];
+    let inicialPaterno = this.apellidoPaterno [0];
+
+
+    return (`${inicialNombre}.${inicialPaterno}.${inicialMaterno}`)
+}
+}
+ let nombre1 = new Nombre ("Jonathan ", "Rodriguez ", "Villaseñor ");
+
+ console.log( nombre1.getNombreCompleto());
+ console.log( nombre1.getApellidoNombre());
+ console.log( nombre1.getIniciales());
